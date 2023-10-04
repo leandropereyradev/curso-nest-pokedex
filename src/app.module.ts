a4e045e6f8058330e9220349955e92f48af69404
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-04 10:32:43 +0200

    Connecting Nest with MongoDB - .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+
     PokemonModule,
   ],
 })
